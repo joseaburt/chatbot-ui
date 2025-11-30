dd6d63b9982c9da4f9d7223c7b9043891629940f
Jose Aburto <89697685+joseaburt@users.noreply.github.com>
2025-12-01 00:54:45 +0100

    feat: implement ElementComponent for dynamic HTML element creation with localization support and unit tests

---
+++ src/ui/factories/component.spec.ts
@@ -0,0 +1,61 @@
+import { DIContainer } from "../../infra/di";
+import { ElementComponent } from "./component";
+import { Locales } from "../../core/types/config";
+import { LocalesService } from "../../infra/locales/locales.service";
+
+describe("ElementComponent", () => {
+  const locales: Locales = {
+    es: {
+      headTitle: "Título de la cabecera",
+      headSubtitle: "Subtítulo de la cabecera",
+      headeMessage: "Mensaje de la cabecera",
+      textboxPlaceholder: "Placeholder del textbox",
+      messageSentError: "Error al enviar el mensaje",
+      defaultMessage: "Mensaje por defecto",
+    },
+    en: {
+      headTitle: "Head Title",
+      headSubtitle: "Head Subtitle",
+      headeMessage: "Head Message",
+      textboxPlaceholder: "Textbox Placeholder",
+      messageSentError: "Message Sent Error",
+      defaultMessage: "Default Message",
+    },
+  };
+
+  DIContainer.register(LocalesService.name, new LocalesService(locales));
+
+  it("should create an element with the correct template", () => {
+    const element = ElementComponent.create({
+      template: "{{textboxPlaceholder}}",
+      className: "header-title",
+      classes: "text-center",
+      as: "h1",
+    });
+    expect(element.getElement().nodeName).toBe("H1");
+    expect(element.getElement().innerHTML).toBe("Placeholder del textbox");
+    expect(element.getElement().classList.contains("header-title")).toBe(true);
+    expect(element.getElement().classList.contains("text-center")).toBe(true);
+  });
+
+  it("should create an element with a child", () => {
+    const element = ElementComponent.create({
+      template: "",
+      className: "header-container",
+      classes: "container",
+      as: "div",
+      childs: [
+        ElementComponent.create({
+          template: "{{textboxPlaceholder}}",
+          className: "header-title",
+          classes: "text-center",
+          as: "h1",
+        }),
+      ],
+    });
+    expect(element.getElement().nodeName).toBe("DIV");
+    expect(element.getElement().classList.contains("header-container")).toBe(true);
+    expect(element.getElement().classList.contains("container")).toBe(true);
+    expect(element.getElement().querySelector("h1")?.innerHTML).toBe("Placeholder del textbox");
+  });
+});
---
+++ src/ui/factories/component.ts
@@ -0,0 +1,57 @@
+import { DIContainer } from "../../infra/di";
+import { LocalesService } from "../../infra/locales/locales.service";
+
+export type BaseElementProps<T extends HTMLElement = HTMLElement> = {
+  template: string;
+  className: string;
+  as: keyof HTMLElementTagNameMap;
+  childs?: Array<ElementComponent>;
+  classes?: string;
+  style?: CSSStyleDeclaration;
+} & Partial<T>;
+
+export class ElementComponent<T extends HTMLElement = HTMLElement> {
+  private template: string = "";
+
+  constructor(private element: T, private readonly props: Omit<BaseElementProps<T>, "as">) {
+    this.element = this.createHelper(this.element, this.props) as T;
+    this.template = this.props.template;
+  }
+
+  private createHelper(element: T, { className, template, childs, classes, ...rest }: Omit<BaseElementProps<T>, "as">) {
+    element.id = this.generateUniqueId();
+    element.classList.add(className);
+    Object.assign(element, rest);
+    const localesService = DIContainer.resolve<LocalesService>(LocalesService.name);
+    element.innerHTML = localesService.translateTemplate(template);
+    if (classes) classes.split(" ").forEach((className) => element.classList.add(className.trim()));
+    if (childs) childs.forEach((child) => this.addChild(child));
+
+    return element;
+  }
+
+  private addChild(child: ElementComponent) {
+    this.element.appendChild(child.getElement());
+    return this;
+  }
+
+  public getElement(): T {
+    return this.element;
+  }
+
+  private generateUniqueId() {
+    return Math.random().toString(36).substring(2, 15);
+  }
+
+  public getTemplate(): string {
+    return this.template;
+  }
+
+  public static create<T extends HTMLElement = HTMLElement>(props: BaseElementProps<T>): ElementComponent<T> {
+    const { as, ...rest } = props;
+    const element = document.createElement(as) as T;
+    const elementComponent = new ElementComponent<T>(element, rest);
+
+    return elementComponent;
+  }
+}
