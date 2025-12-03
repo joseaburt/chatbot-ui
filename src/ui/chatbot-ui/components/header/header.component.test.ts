bb32f508a84977a31b9baf183cd7a5257c4c59fe
Jose Aburto <89697685+joseaburt@users.noreply.github.com>
2025-12-03 21:35:31 +0100

    feat: implement HeaderComponent with localization support and state management for open/close functionality

---
+++ src/ui/chatbot-ui/components/header/header.component.test.ts
@@ -0,0 +1,31 @@
+import { locales } from "../../../../configs";
+import { createHeader } from "./header.component";
+import { resolveDependency } from "../../../../di";
+import userEvent from "@testing-library/user-event";
+import { getByRole, getByText } from "@testing-library/dom";
+import { createHtmlContainer } from "../../../../test/test.utils";
+
+const stateController = resolveDependency("StateController");
+
+describe("HeaderComponent", () => {
+  it("should create a header component", () => {
+    let container = createHtmlContainer(() => createHeader().getElement(), { lang: "es" });
+
+    expect(getByText(container, locales.es.headTitle)).toBeDefined();
+    expect(getByText(container, locales.es.headSubtitle)).toBeDefined();
+    expect(getByText(container, locales.es.headMessage)).toBeDefined();
+
+    container = createHtmlContainer(() => createHeader().getElement(), { lang: "en" });
+    expect(getByText(container, locales.en.headTitle)).toBeDefined();
+    expect(getByText(container, locales.en.headSubtitle)).toBeDefined();
+    expect(getByText(container, locales.en.headMessage)).toBeDefined();
+  });
+
+  it("should toggle isOpen state when close button is clicked", async () => {
+    let container = createHtmlContainer(() => createHeader().getElement(), { lang: "es" });
+    const closeButton = getByRole(container, "button");
+    expect(stateController.getState().isOpen).toBe(true);
+    await userEvent.click(closeButton);
+    expect(stateController.getState().isOpen).toBe(false);
+  });
+});
---
+++ src/ui/chatbot-ui/components/header/header.component.ts
@@ -0,0 +1,49 @@
+import { Close, Sparkles } from "../../../icons";
+import { resolveDependency } from "../../../../di";
+import { ElementComponent } from "../../../factories/component";
+
+const stateController = resolveDependency("StateController");
+
+export const createHeader = () => {
+  return ElementComponent.create({
+    as: "header",
+    template: "",
+    className: "header",
+    classes: "bg-primary p-3",
+    childs: [
+      ElementComponent.create({
+        template: `<div><span></span></div>`,
+        className: "header-header",
+        classes: "flex items-center justify-between color-white",
+        as: "div",
+        childs: [
+          ElementComponent.create<"div">({
+            template: `${Sparkles} <h2 class="text-16 text-semibold">{{headTitle}}</h2>`,
+            className: "heading-title",
+            classes: "text-16 text-bold flex items-center gap-2",
+            as: "div",
+          }),
+          ElementComponent.create<"button">({
+            as: "button",
+            template: Close,
+            className: "close-button",
+            classes: "size-8 color-white",
+            disabled: false,
+            onclick: () => {
+              stateController.setState((pre) => ({ ...pre, isOpen: !pre.isOpen }));
+            },
+          }),
+        ],
+      }),
+      ElementComponent.create({
+        template: `<div class="flex flex-col gap-1 color-white w-full text-center mt-1">
+        <p class="text-14 text-semibold">{{headSubtitle}}</p>
+        <p class="text-12 text-normal">{{headMessage}}</p>
+      </div>
+      `,
+        className: "header-subtitle",
+        as: "div",
+      }),
+    ],
+  });
+};
